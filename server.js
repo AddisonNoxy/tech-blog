c32eae28b63c289a56455cd334eb50cc3d3231d1
Addison Noxy <drolknil@gmail.com>
2023-08-01 23:00:45 -0400

    initial handlebars files

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+const routes = require('./routes');
+//const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create();
+
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log("Server listening!");
+})
